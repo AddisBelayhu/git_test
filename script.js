6ed1fe69709b32234becb7444cd3d13aacd28099
AddisBelayhu <addisgithub@gmail.com>
2023-03-13 16:55:38 +0300

    Git push effect

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 
 
-
 function buttonEffect(){
 
     const btn = document.getElementById("btnOne");
     btn.innerHTML = "This is the First git hub push project";
+}
+
+function mouthOver(){
+    const mouseEffect = document.getElementById("circleOne");
+    mouseEffect.innerHTML= "Pink";
 }
 
